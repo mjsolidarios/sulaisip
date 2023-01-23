5c9205dda9639b4e2411378676c38b17750961e7
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2023-01-23 18:03:44 +0800

    Text completion feature

--- app/Main.gd
+++ app/Main.gd
@@ -13,37 +13,49 @@ func _ready():
 		button.text = i
 		button.connect("pressed", self, "_add_text_to_input", [i])
 		$VBoxContainer/GridContainer.add_child(button)
+	
+	$VBoxContainer/Button.emit_signal("gui_input")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	Input.warp_mouse_position(Vector2(0,0))
+	
 func _add_text_to_input(var character):
 	$VBoxContainer/TextEdit.text += character
 	_predict_next_character()
 
-func _predict_next_character():
+func _predict_next_character(text_fill:=""):
 	var current_text = $VBoxContainer/TextEdit.text
-#	print(current_text)
+	if text_fill.length() > 0:
+		$VBoxContainer/TextEdit.text = current_text+text_fill+" "
 	var url = "http://127.0.0.1:5000"
 	var headers = ["Content-Type: application/json"]
 	var use_ssl=true
 	
-	var query := { "text": current_text }
+	var query := { "text": $VBoxContainer/TextEdit.text }
 	
 	if current_text.length()>0:
-		# print("current_text.length(): "+str(current_text.length()))
-		# print(query)
-		$HTTPRequest.request(url, headers, false, HTTPClient.METHOD_POST, to_json(query))
+		$HTTPRequest.request(url, headers, use_ssl, HTTPClient.METHOD_POST, to_json(query))
 	else:
 		$HTTPRequest.request(url)
 
 func _on_HTTPRequest_request_completed(result, response_code, headers, body):
-	print(result)
 	var json = JSON.parse(body.get_string_from_utf8())	
-	print(json.result['nextCharacters'])
-	print(json.result['stringGroup'])
-	print(json.result['wordList'])
-
-
+#	print(json.result['nextCharacters'])
+#	print(json.result['stringGroup'])
+#	print(json.result['wordList'])
+	
+	# clear node
+	var TextSuggestionsChildren = $VBoxContainer/TextSuggestions.get_children()
+	for i in TextSuggestionsChildren:
+		$VBoxContainer/TextSuggestions.remove_child(i)
+		
+	for i in json.result['wordList']:
+		var TextButton = Button.new()
+		var TrimmedText = i['sequence'].replace(".", "")
+		TextButton.text = TrimmedText
+		TextButton.connect("pressed", self, "_predict_next_character", [i['token_str']])
+		$VBoxContainer/TextSuggestions.add_child(TextButton)
+		
 func _on_Button_pressed():
 	_predict_next_character()
