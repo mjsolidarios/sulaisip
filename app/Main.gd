a2a959a19094f5d4ac4067ffd407a7ef716e5f92
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2023-02-09 17:46:50 +0800

    Additional sensors

--- app/Main.gd
+++ app/Main.gd
@@ -23,14 +23,21 @@ export var active_button_top_index = 0
 export var active_button_bottom_grid_index = 0
 
 
-onready var text_suggestions_node = get_node("VBoxContainer/NinePatchRect/MarginContainer/TextSuggestions")
-onready var button_keys_node = get_node("VBoxContainer/NinePatchRect2/MarginContainer/HBoxContainer")
-onready var button_char_keys_node = get_node("VBoxContainer/NinePatchRect2/MarginContainer/HBoxContainer/GridContainer")
+onready var text_suggestions_node = get_node("HBoxContainer/VBoxContainer/NinePatchRect/MarginContainer/TextSuggestions")
+onready var button_keys_node = get_node("HBoxContainer/VBoxContainer/NinePatchRect2/MarginContainer/HBoxContainer")
+onready var button_char_keys_node = get_node("HBoxContainer/VBoxContainer/NinePatchRect2/MarginContainer/HBoxContainer/GridContainer")
 onready var char_grid = button_char_keys_node
 
 var timer_started = false
 var timer_pressed_started = false
 var active_direction = "right"
+
+var gyro = Vector3(0,0,0)
+var acce = Vector3(0,0,0)
+var mag = Vector3(0,0,0)
+var grav = Vector3(0,-1,0)
+
+var last_rotation = 0
 
 func _array_to_string(arr: Array) -> String:
 	var s = ""
@@ -61,12 +68,16 @@ func _moveto_next_button():
 			active_button_top_index -= 1
 		else:
 			active_button_bottom_grid_index -= 1
-	else:
+	if active_direction=="right":
 		if active_group == "top":
 			active_button_top_index += 1
 		else:
 			active_button_bottom_grid_index += 1
-			
+	if active_direction=="neutral":
+		pass
+	
+	# Reset
+	
 	if active_button_top_index > 4:
 		active_button_top_index = 0
 	if active_button_bottom_grid_index > 7:
@@ -76,9 +87,9 @@ func _moveto_next_button():
 	
 
 func _del_char():
-	var text = $VBoxContainer/TextEdit.text
+	var text = $HBoxContainer/VBoxContainer/TextEdit.text
 	if text.length() > 0:
-		$VBoxContainer/TextEdit.text = text.substr(0, text.length() - 1)
+		$HBoxContainer/VBoxContainer/TextEdit.text = text.substr(0, text.length() - 1)
 	_predict_next_character()
 
 func _create_control_button(_type):
@@ -170,25 +181,98 @@ func _ready():
 	
 	file.open("res://data/character_rankings.json", File.READ)
 	character_frequency_data = parse_json(file.get_as_text())
-	$VBoxContainer/TextEdit.text = " "
+	$HBoxContainer/VBoxContainer/TextEdit.text = " "
 	_predict_next_character()
 	print(_wsclient)
 	# websocket
 	
 func _exit_tree():
 	thread.wait_to_finish()
+
+func calc_north(p_grav, p_mag):
+	# Always use normalized vectors!
+	p_grav = p_grav.normalized()
+	
+	# Calculate east (or is it west) by getting our cross product.
+	# The cross product of two normalized vectors returns a vector that
+	# is perpendicular to our two vectors
+	var east = p_grav.cross(p_mag.normalized()).normalized()
+	
+	# Cross again to get our horizon aligned north
+	return east.cross(p_grav).normalized()
+
+func orientate_by_mag_and_grav(p_mag, p_grav):
+	var rotate = Basis()
+	
+	# as always, normalize!
+	p_mag = p_mag.normalized()
+	
+	# gravity points down, so - gravity points up!
+	rotate.y = -p_grav.normalized()
+	
+	# Cross products with our magnetic north gives an aligned east (or west, I always forget)
+	rotate.x = rotate.y.cross(p_mag)
+	
+	# And cross product again and we get our aligned north completing our matrix
+	rotate.z = rotate.x.cross(rotate.y)
+	
+	return rotate
+
+func rotate_by_gyro(p_gyro, p_basis, p_delta):
+	var rotate = Basis()
+	
+	rotate = rotate.rotated(p_basis.x, -p_gyro.x * p_delta)
+	rotate = rotate.rotated(p_basis.y, -p_gyro.y * p_delta)
+	rotate = rotate.rotated(p_basis.z, -p_gyro.z * p_delta)
+	
+	return rotate * p_basis
+
+func drift_correction(p_basis, p_grav):
+	# as always, make sure our vector is normalized but also invert as our gravity points down
+	var real_up = -p_grav.normalized()
+	
+	# start by calculating the dot product, this gives us the cosine angle between our two vectors
+	var dot = p_basis.y.dot(real_up)
+	
+	# if our dot is 1.0 we're good
+	if (dot < 1.0):
+		# the cross between our two vectors gives us a vector perpendicular to our two vectors
+		var axis = p_basis.y.cross(real_up).normalized()
+		var correction = Basis(axis, acos(dot))
+		p_basis = correction * p_basis
+	
+	return p_basis
 	
 func _input(event):
 	pass
 
+func get_basis_for_arrow(p_vector):
+	var rotate = Basis()
+
+	# as our arrow points up, Y = our direction vector
+	rotate.y = p_vector.normalized()
+
+	# get an arbitrary vector we can use to calculate our other two vectors
+	var v = Vector3(1.0, 0.0, 0.0)
+	if abs(v.dot(rotate.y)) > 0.9:
+		v = Vector3(0.0, 1.0, 0.0)
+
+	# use our vector to get a vector perpendicular to our two vectors
+	rotate.x = rotate.y.cross(v).normalized()
+
+	# and the cross product again gives us our final vector perpendicular to our previous two vectors
+	rotate.z = rotate.x.cross(rotate.y).normalized()
+
+	return rotate
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if active_group == "top":
-		$VBoxContainer/NinePatchRect.is_active = true
-		$VBoxContainer/NinePatchRect2.is_active = false
+		$HBoxContainer/VBoxContainer/NinePatchRect.is_active = true
+		$HBoxContainer/VBoxContainer/NinePatchRect2.is_active = false
 	else:
-		$VBoxContainer/NinePatchRect.is_active = false
-		$VBoxContainer/NinePatchRect2.is_active = true
+		$HBoxContainer/VBoxContainer/NinePatchRect.is_active = false
+		$HBoxContainer/VBoxContainer/NinePatchRect2.is_active = true
 	
 	var children_top_count = text_suggestions_node.get_child_count()
 	var children_bottom_count = button_keys_node.get_child_count()
@@ -206,9 +290,34 @@ func _process(delta):
 			if  i.get_index() == active_button_bottom_grid_index:
 				i.state="hover"
 			else:
-				i.state="normal"
+				i.state="normal" 
 	
 	_wsclient.poll()
+
+# Update our arrow showing gravity
+	get_node("HBoxContainer/Panel/ViewportContainer/Viewport/Spatial/Arrows/AccelerometerArrow").transform.basis = get_basis_for_arrow(acce)
+
+	# Update our arrow showing our magnetometer
+	# Note that in absense of other strong magnetic forces this will point to magnetic north, which is not horizontal thanks to the earth being, uhm, round
+	get_node("HBoxContainer/Panel/ViewportContainer/Viewport/Spatial/Arrows/MagnetoArrow").transform.basis = get_basis_for_arrow(mag)
+
+	# Calculate our north vector and show that
+	var north = calc_north(acce,mag)
+	get_node("HBoxContainer/Panel/ViewportContainer/Viewport/Spatial/Arrows/NorthArrow").transform.basis = get_basis_for_arrow(north)
+	
+	
+	if mag.length() < 0.1:
+		mag = Vector3(1.0, 0.0, 0.0)
+	
+	if acce.length() < 0.1:
+		acce = Vector3(0.0, -1.0, 0.0)
+	
+	var mesh = $HBoxContainer/Panel/ViewportContainer/Viewport/Spatial/MeshInstance
+	var new_basis = rotate_by_gyro(gyro, mesh.transform.basis, delta).orthonormalized()
+	mesh.transform.basis = drift_correction(new_basis, acce)
+	
+	var mesh2 = $HBoxContainer/Panel/ViewportContainer/Viewport/Spatial/MeshInstance2
+	mesh2.transform.basis = orientate_by_mag_and_grav(mag, acce).orthonormalized()
 
 func _wsconnected(proto = ""):
 	print("Connected with protocol: ", proto)
@@ -224,57 +333,90 @@ func _wserror(was_clean = false):
 func _wsondata():
 	
 	var data = _wsclient.get_peer(1).get_packet().get_string_from_utf8()
-	var parsed_data = data.replace("[", "").replace("]", "").split(",")
-
-	var blink = parsed_data[0].to_float()
-	var left = parsed_data[1].to_float()
-	var right = parsed_data[2].to_float()
-	var aX = abs(parsed_data[3].to_float())
-	var aY = abs(parsed_data[4].to_float())
-	var aZ = abs(parsed_data[5].to_float())
+	var parsed_data = parse_json(data)
+#	var parsed_data = data.replace("[", "").replace("]", "").split(",")
 	
-	print("left: "+str(left),"|","right: "+str(right)) 
+	var blink = parsed_data.blink
+	var left = 0
+	var right = 0
+	var push = 0
+	if parsed_data.has("aL"):
+		left = parsed_data.aL
+	if parsed_data.has("aR"):
+		right = parsed_data.aR
+	if parsed_data.has("push"):
+		right = parsed_data.push
+	var gX = parsed_data.gX
+	var gY = parsed_data.gY
+	var gZ = parsed_data.gZ
+	var aX = parsed_data.aX
+	var aY = parsed_data.aY
+	var aZ = parsed_data.aZ
+	var mX = parsed_data.mX
+	var mY = parsed_data.mY
+	var mZ = parsed_data.mZ
+	gyro = Vector3(gX, gY, gZ)
+	acce = Vector3(aX, aY, aZ)
+	mag = Vector3(mX, mY, mZ)
+	
+	# print("left: "+str(left),"|","right: "+str(right)) 
 
-	if left > 0:
-		active_direction = "left"
-		if !timer_started:
-			$Timer.start()
-			timer_started = true
-	if right > 0:
+#	if left > 0.8:
+#		active_direction = "left"
+#		if !timer_started:
+#			$Timer.start()
+#			timer_started = true
+#	if right > 0.8:
+#		active_direction = "right"
+#		if !timer_started:
+#			$Timer.start()
+#			timer_started = true
+	
+	# print("left: "+str(left)+"|"+"right: "+str(right))
+		
+#	if blink > 0:
+#		if !timer_pressed_started:
+#			$TimerPressButton.start()
+#			timer_pressed_started = true
+#		blink = 0
+	
+	var pitch = (atan2(-gX, gZ) * 180 / PI )
+	var roll = abs(atan2(-aY, aZ) * 180 / PI)
+	
+	if roll > 100:
+		active_group = "bottom"
+	else:
+		active_group = "top"
+	
+	var current_rotation = $HBoxContainer/Panel/ViewportContainer/Viewport/Spatial/MeshInstance.get_rotation_degrees().x
+	
+	var threshhold = 10 
+	
+	print(left)
+	 
+	if current_rotation > last_rotation + threshhold:
 		active_direction = "right"
 		if !timer_started:
 			$Timer.start()
 			timer_started = true
-		
-	if blink != 0 and blink == 100:
-		if !timer_pressed_started:
-			$TimerPressButton.start()
-			timer_pressed_started = true
-		blink = 0
-	
-	var pitch = abs(atan2(-aX, aZ) * 180 / PI )
-	var roll = abs(atan2(-aY, aZ) * 180 / PI)
-	var pitch_to_rad = deg2rad(pitch)
-	var roll_to_rad = deg2rad(roll)
-
-	if roll > 20 and roll != 90:
-		#print(roll_to_rad)
-		if roll_to_rad > 1:
-			active_group = "top"
-		else:
-			active_group = "bottom"
+	elif current_rotation < last_rotation + threshhold:
+		active_direction = "left"
+		if !timer_started:
+			$Timer.start()
+			timer_started = true
+	last_rotation = current_rotation
 	
 func _add_text_to_input(var character):
-	$VBoxContainer/TextEdit.text += character
+	$HBoxContainer/VBoxContainer/TextEdit.text += character
 	_predict_next_character()
 
 func _predict_next_character(text_fill:=""):
-	var current_text = $VBoxContainer/TextEdit.text
+	var current_text = $HBoxContainer/VBoxContainer/TextEdit.text
 	if text_fill.length() > 0:
-		$VBoxContainer/TextEdit.text = current_text+text_fill+" "
+		$HBoxContainer/VBoxContainer/TextEdit.text = current_text+text_fill+" "
 	
-	if $VBoxContainer/TextEdit.text.length() > 0:
-		query = { "text": $VBoxContainer/TextEdit.text }
+	if $HBoxContainer/VBoxContainer/TextEdit.text.length() > 0:
+		query = { "text": $HBoxContainer/VBoxContainer/TextEdit.text }
 	else:
 		query = { "text": "" }
 	
